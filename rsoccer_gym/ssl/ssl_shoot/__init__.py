5866efdd6c7c6f7a5378536bd200a5875e1f0a7b
JustinRodney <justinandrewrodney@gmail.com>
2022-02-06 15:29:47 -0500

    Added shoot behaviors

---
+++ rsoccer_gym/ssl/ssl_shoot/__init__.py
@@ -0,0 +1,5 @@
+from rsoccer_gym.ssl.ssl_shoot.ssl_gym_shoot_short_new import SSLShootShortNewEnv
+from rsoccer_gym.ssl.ssl_shoot.ssl_gym_shoot import SSLShootEnv
+from rsoccer_gym.ssl.ssl_shoot.ssl_gym_shoot_cmu import SSLShootCMUEnv
+from rsoccer_gym.ssl.ssl_shoot.ssl_gym_shoot_short2 import SSLShootShort2Env
+from rsoccer_gym.ssl.ssl_shoot.ssl_gym_shoot_best import SSLShootBestEnv
