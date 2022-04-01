4b548e40cfc7e286197c960262ef876f7fbdc027
justinandrewrodney <justinandrewrodney@gmail.com>
2022-03-31 21:56:53 -0400

    add init

--- rsoccer_gym/__init__.py
+++ rsoccer_gym/__init__.py
@@ -27,7 +27,7 @@ register(id='VSSFIRA-v0',
 
 register(id='SSLGoToBall-v0',
          entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallEnv',
-         kwargs={'field_type': 2, 'n_robots_yellow': 6},
+         kwargs={'field_type': 2, 'n_robots_yellow': 0},
          max_episode_steps=1200
          )
 
@@ -39,15 +39,59 @@ register(id='SSLGoToBallIR-v0',
 
 register(id='SSLGoToBallDDPG-v0',
          entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallDDPGEnv',
-         kwargs={'field_type': 2, 'n_robots_yellow': 0},
+         kwargs={'field_type': 1, 'n_robots_yellow': 0},
          max_episode_steps=330
          )
 
 register(id='SSLGoToBallDDPGLocal-v0',
          entry_point='rsoccer_gym.ssl.ssl_go_to_ball:SSLGoToBallDDPGLocalEnv',
-         kwargs={'field_type': 2, 'n_robots_yellow': 0},
+         kwargs={'field_type': 1, 'n_robots_yellow': 0},
          max_episode_steps=330
          )
+
+register(id='SSLShoot-v0',
+         entry_point='rsoccer_gym.ssl.ssl_shoot:SSLShootEnv',
+         kwargs={'field_type': 1, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=100
+         )
+
+register(id='SSLShootCMU-v0',
+         entry_point='rsoccer_gym.ssl.ssl_shoot:SSLShootCMUEnv',
+         kwargs={'field_type': 1, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=100
+         )
+
+
+register(id='SSLGoalie-v0',
+         entry_point='rsoccer_gym.ssl.ssl_goalie:SSLGoalieEnv',
+         kwargs={'field_type': 1, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=100
+         )
+
+register(id='SSLShootShort2-v0',
+         entry_point='rsoccer_gym.ssl.ssl_shoot:SSLShootShort2Env',
+         kwargs={'field_type': 2, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=58
+         )
+
+register(id='SSLShootShortNew-v0',
+         entry_point='rsoccer_gym.ssl.ssl_shoot:SSLShootShortNewEnv',
+         kwargs={'field_type': 1, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=100
+         )
+
+register(id='SSLShootBest-v0',
+         entry_point='rsoccer_gym.ssl.ssl_shoot:SSLShootBestEnv',
+         kwargs={'field_type': 1, 'random_init': True,
+                 'enter_goal_area': False},
+         max_episode_steps=100
+         )
+
 
 register(id='SSLGoToBallShoot-v0',
          entry_point='rsoccer_gym.ssl.ssl_go_to_ball_shoot:SSLGoToBallShootEnv',
